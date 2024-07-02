f523f2f5ffefed262dcb483466e5cafd1f74cfab
duyvu997 <ngocduy0997@gmail.com>
2024-07-02 15:52:13 +0700

    add env var

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,16 +1,17 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 import type { NormalizedCacheObject } from '@apollo/client';
 
+const uri = process.env.NEXT_PUBLIC_GRAPH_QL_URL || 'http://localhost:3000/api';
 // Function to create a new Apollo Client instance
 const createApolloClient = (): ApolloClient<NormalizedCacheObject> => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: "http://localhost:4000/",
+      uri,
     }),
   });
 };
 
 // Register the Apollo Client with Next.js experimental support
-export default createApolloClient
+export default createApolloClient;
