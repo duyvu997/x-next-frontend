c7960e06386b12ef8be16e5a849a542afce11dd8
duyvu997 <ngocduy0997@gmail.com>
2024-07-02 14:42:29 +0700

    add noti

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css';
 
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import { ToastProvider } from 'react-toast-notifications';
 import createApolloClient from '../lib/client';
 
 const client = createApolloClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ToastProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ToastProvider>
   );
 }
